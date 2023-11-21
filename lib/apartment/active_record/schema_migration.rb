79872894598adf591083e8a9b3d8952c2cb04657
Marek Zieliński <marek.zielinski@0xb8.net>
2023-11-21 13:32:32 +0100

    just disable the SchemaMigration patch

--- lib/apartment/active_record/schema_migration.rb
+++ lib/apartment/active_record/schema_migration.rb
@@ -1,16 +1,29 @@
 # frozen_string_literal: true
 
-module ActiveRecord
-  class SchemaMigration # :nodoc:
-    class << self
-      def table_exists?
-        connection.table_exists?(table_name)
-      end
+# module ActiveRecord
+#   class SchemaMigration # :nodoc:
+#     class << self
+#       def table_exists?
+#         connection.data_source_exists?(table_name)
+#       end
+#
+#       alias :table_name :name
+#     end
+#   end
+# end
 
-      alias :table_name :name
-    end
-  end
-end
+# module ActiveRecord
+#   module ConnectionAdapters # :nodoc:
+#     module SchemaStatements
+#       def data_source_exists?(name)
+#         pp " name: #{name}, data_source_sql: #{data_source_sql(name)}"
+#         query_values(data_source_sql(name), "SCHEMA").any? if name.present?
+#       rescue NotImplementedError
+#         data_sources.include?(name.to_s)
+#       end
+#     end
+#   end
+# end
 
 # TODO: required only for ransack < 4.1
 class Arel::Table
